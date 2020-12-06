8fa3acf4a4dfa55fc9c547f4be30b5bd7c98a221
zdszero <1335908147@qq.com>
2020-12-06 12:39:21 +0800

    update

---
+++ gen_outline.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python
+
+import os
+
+OUTPUT_FILE = open('./index.md', 'a')
+
+def walk_dir(rel_path: str, dep: int):
+    for file_name in os.listdir(rel_path):
+        file_path = rel_path + '/' + file_name
+        if not file_name.startswith('.') and file_name != 'img' and os.path.isdir(file_path):
+            OUTPUT_FILE.write('{} [{}]({})'.format('#' * dep, file_name, file_path) + os.linesep * 2)
+            print('{} [{}]({})'.format('#' * dep, file_name, file_path) + os.linesep * 2, end='')
+            walk_dir(file_path, dep + 1)
+        elif file_name.endswith('md'):
+            OUTPUT_FILE.write('+ [{}]({})'.format(file_name, file_path) + os.linesep)
+            print('+ [{}]({})'.format(file_name, file_path) + os.linesep, end='')
+    print('')
+
+walk_dir('.', 1)
